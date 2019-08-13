a6aac3be4f6fb02c10f0331ba569f2ad36598e99
Tylor Schafer <tylorschafer@gmail.com>
2019-08-12 21:53:45 -0600

    Setup Merchants DB, routes, and controller

--- spec/features/merchants/index_spec.rb
+++ spec/features/merchants/index_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-Rspec.describe 'merchants index page', type: :feature do
+RSpec.describe 'merchants index page', type: :feature do
   it 'user can see all songs' do
     merchant_1 = Merchant.create(name: "Rick's Risky Tire Euporium")
     merchant_2 = Merchant.create(name: "Wild Wild Western Fabrics")
 
-    vist "/merchants"
+    visit "/merchants"
 
     expect(page).to have_content(merchant_1.name)
     expect(page).to have_content(merchant_2.name)
