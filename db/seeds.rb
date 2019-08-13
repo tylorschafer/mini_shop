be73c74207ae65cb5a1bd2844c4f18fdd50c8fd4
Tylor Schafer <tylorschafer@gmail.com>
2019-08-13 12:58:49 -0600

    Add new columns to merchants, reseed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,18 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Merchant.delete_all
 
-Merchant.create(name: "Rick's Risky Tire Euporium")
-Merchant.create(name: "Wild Wild Western Fabrics")
+Merchant.create(
+  name: "Rick's Risky Tire Euporium",
+  address: "123 Fake Lane",
+  city: "Fakeville",
+  state: "Colorado",
+  zip: "80203"
+)
+Merchant.create(
+  name: "Wild Wild Western Fabrics",
+  address: "456 Not Real Ct",
+  city: "False Town",
+  state: "Michigan",
+  zip: "20356"
+)
