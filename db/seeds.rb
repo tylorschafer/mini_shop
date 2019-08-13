43106570253d9bf94358dba6a79310ff288b90c4
Tylor Schafer <tylorschafer@gmail.com>
2019-08-13 15:52:24 -0600

    Update seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-Merchant.delete_all
+Merchant.destroy_all
 
 Merchant.create(
   name: "Rick's Risky Tire Euporium",
