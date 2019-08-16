ff77af417ed205585cada6b0f760cc0842d93054
Tylor Schafer <tylorschafer@gmail.com>
2019-08-15 22:14:43 -0600

    Add item association testing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+  
+  validates_presence_of :name, :description, :price, :image, :active_status, :inventory, :merchant_id
 end
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe Item, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :active_status }
+    it { should validate_presence_of :inventory }
+    it { should validate_presence_of :merchant_id }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :merchant }
+  end
+end
