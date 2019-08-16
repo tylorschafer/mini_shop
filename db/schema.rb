d8e9aa8606ffc89b3767844533d2c989b210cf3c
Tylor Schafer <tylorschafer@gmail.com>
2019-08-15 20:13:59 -0600

    Add merchant_id to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190816020947) do
+ActiveRecord::Schema.define(version: 20190816021302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20190816020947) do
     t.string "image"
     t.boolean "active_status"
     t.integer "inventory"
+    t.bigint "merchant_id"
+    t.index ["merchant_id"], name: "index_items_on_merchant_id"
   end
 
   create_table "merchants", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema.define(version: 20190816020947) do
     t.string "zip"
   end
 
+  add_foreign_key "items", "merchants"
 end
