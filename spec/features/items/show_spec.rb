a1cdcaaf5a805e099d24f29d3d54d6d8d7d8857e
Tylor Schafer <tylorschafer@gmail.com>
2019-08-17 15:14:28 -0600

    Add testing for edit item

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,32 +1,39 @@
 require 'rails_helper'
 
 describe 'Items show page' do
-  it "I can see that specific item's details" do
-    ricks = Merchant.create(
+  before :each do
+    @ricks = Merchant.create(
       name: "Rick's Risky Tire Euporium",
       address: "123 Fake Lane",
       city: "Fakeville",
       state: "Colorado",
       zip: "80203"
     )
-    item = Item.create(
+    @item = Item.create(
       name: 'Super Tread 9000',
       description: 'A real nice tire',
       price: 9999,
       image:'https://images.tirebuyer.com/visual-aids/products/tires/falken/wildpeakat3w/falken_wildpeakat3w_rbl_341933_sidewall_jpg_s3_resize_64x64_sidewall.jpg',
       active_status: true,
       inventory: 657,
-      merchant_id: ricks.id
+      merchant_id: @ricks.id
     )
+  end
+  it "I can see that specific item's details" do
 
-    visit "/items/#{item.id}"
+    visit "/items/#{@item.id}"
 
-    expect(page).to have_content(item.name)
+    expect(page).to have_content(@item.name)
     expect(page).to have_content("Active = True")
     expect(page).to have_content("Price: $9,999.00")
-    expect(page).to have_css("img[src*='#{item.image}']")
-    expect(page).to have_content("Description: #{item.description}")
-    expect(page).to have_content("Stock: #{item.inventory}")
-    expect(page).to have_content("Merchant: #{ricks.name}")
+    expect(page).to have_css("img[src*='#{@item.image}']")
+    expect(page).to have_content("Description: #{@item.description}")
+    expect(page).to have_content("Stock: #{@item.inventory}")
+    expect(page).to have_content("Merchant: #{@ricks.name}")
+  end
+  it 'Has a link to update item' do
+    visit "/items/#{@item.id}"
+
+    expect(page).to have_link('Update Item')
   end
 end
